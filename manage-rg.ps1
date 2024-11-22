c866e86ce97dba3e539003668b225211b7aef485
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2024-11-22 09:01:34 +0000

    Add files via upload

---
+++ manage-rg.ps1
@@ -0,0 +1,39 @@
+$resourceGroupName = "myResourceGroup"
+$location = "uksouth"
+
+
+# creation of rg
+function Create-ResourceGroup {
+    Write-Host "Creating resource group '$ResourceGroupName' in location '$Location'..."
+    New-AzResourceGroup -Name $resourceGroupName -Location $location
+    Write-Host "resource group '$resourceGroupName' created."
+}
+
+
+
+
+
+# deletion of resource group
+function Delete-ResourceGroup {
+    Write-Host "Deleting resource group '$resourceGroupName'..."
+    Remove-AzResourceGroup -Name $resourceGroupName -Force
+    Write-Host "'$resourceGroupName' deleted"
+}
+
+
+
+# choice of delete or create
+Write-Host "choose:"
+Write-Host "create rg"
+Write-Host "delete rg"
+$choice = Read-Host "enter choice"
+
+if ($choice -eq 1) {
+    Create-ResourceGroup
+} elseif ($choice -eq 2) {
+    Delete-ResourceGroup
+} else {
+    Write-Host "invalid..."
+}
+
+Write-Host "script done"
